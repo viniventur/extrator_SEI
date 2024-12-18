f00ea976ce7bb6cf8ce7898caafe4ebf2445e9f6
Vinicius Ventura <viniciuscventura@gmail.com>
2024-12-18 14:46:40 -0300

    path chromedriver

--- chrome.py
+++ chrome.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
 
 def chrome():
     chrome_options = Options()
@@ -12,5 +10,5 @@ def chrome():
     chrome_options.add_experimental_option('detach', True)
     chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 
-    driver = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
+    driver = webdriver.Chrome(options=chrome_options, service=Service(executable_path='./chromedriver.exe'))
     return driver
