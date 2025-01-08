84198f7f15994ff4badc3ebd7807f7c3e64d8c57
Vinicius Ventura <viniciuscventura@gmail.com>
2025-01-08 14:18:12 -0300

    Adicao de link de acesso ao processo, coluna de concluido e unidade aberta

--- scraping/scrapping_processos.py
+++ scraping/scrapping_processos.py
@@ -20,6 +20,23 @@ from io import BytesIO
 import re
 
 
+def mudar_iframe(iframe):
+
+    driver = st.session_state.driver
+
+    if (iframe == 'arvore'):
+
+        driver.switch_to.default_content()
+        iframe_arvore = driver.find_element('name', "ifrArvore")
+        driver.switch_to.frame(iframe_arvore)
+
+    elif (iframe == 'visualizacao'):
+
+        driver.switch_to.default_content()
+        iframe_visualizacao = driver.find_element('name', "ifrVisualizacao")
+        driver.switch_to.frame(iframe_visualizacao)
+
+
 def converter_para_excel(df_processos):
     output = BytesIO()
     with pd.ExcelWriter(output, engine='openpyxl') as writer:
@@ -50,7 +67,7 @@ def buscar_dados_andamento(unidade, processos):
     cronometro_texto = st.empty()  # Espaço para o cronômetro
 
     # Calcula e exibe o tempo estimado
-    tempo_estimado_total = total_processos * 4  # 6 segundos por processo
+    tempo_estimado_total = total_processos * 6  # 6 segundos por processo
     minutos_estimados, segundos_estimados = divmod(tempo_estimado_total, 60)
     tempo_estimado_formatado = f"{int(minutos_estimados)}min {int(segundos_estimados)}s"
     st.write(f"Tempo estimado para concluir a busca: {tempo_estimado_formatado}")
@@ -62,8 +79,8 @@ def buscar_dados_andamento(unidade, processos):
 
         driver.find_element("xpath", '//*[@id="selInfraUnidades"]').send_keys(unidade)
 
-        colunas_sem_processo = ['Data Horário', 'Qnt. Dias', 'Unidade Atual',
-            'Nome Unidade Atual', 'Usuário CPF', 'Usuário', 'Descrição'
+        colunas_sem_processo = ['Data Horário', 'Qnt. Dias', 'Concluído', 'Unidade Atual',
+            'Nome Unidade Atual', 'Usuário CPF', 'Usuário', 'Descrição', 'Processo aberto nas unidades', 'Link do Processo'
             ]
 
         colunas = ['Processos'] + colunas_sem_processo
@@ -90,6 +107,10 @@ def buscar_dados_andamento(unidade, processos):
             driver.find_element("xpath", '//*[@id="txtPesquisaRapida"]').send_keys(Keys.ENTER)
             time.sleep(tempo_curto)
 
+            # =============================================
+            # VERIFICANDO A EXISTENCIA OU ACESSO AO PROCESSO
+            # =============================================
+
             # Verificar se o processo não foi encontrado
             try:
                 elemento_nao_encontrado = driver.find_element("xpath", '//*[@id="sbmPesquisar"]')
@@ -101,19 +122,15 @@ def buscar_dados_andamento(unidade, processos):
 
             # Alternar para o iframe 'ifrArvore'
             try:
-                iframe_arvore = driver.find_element('name', "ifrArvore")
-                driver.switch_to.frame(iframe_arvore)
-                driver.find_element("xpath", '//*[@id="divConsultarAndamento"]/a/span').click()  # Consultar andamento
+                mudar_iframe('arvore')
+                driver.find_element("xpath", '//*[@id="divConsultarAndamento"]/a/span')  # Consultar andamento
             except:
                 driver.switch_to.default_content()
                 processos.loc[processos['Processos'] == processo, colunas_sem_processo] = "Unidade Não Possui Acesso"
                 continue
 
             # Alternar para o iframe 'ifrVisualizacao'
-            driver.switch_to.default_content()
-            iframe_visualizacao = driver.find_element('name', "ifrVisualizacao")
-            driver.switch_to.frame(iframe_visualizacao)
-            
+            mudar_iframe('visualizacao')
             time.sleep(tempo_medio)
             
             # Verificar se a unidade não tem acesso ao processo
@@ -126,7 +143,63 @@ def buscar_dados_andamento(unidade, processos):
             except:
                 pass  # Unidade tem acesso, continuar
 
-            # DADOS:
+            # =============================================
+            # PROCESSO DE RASPAGEM
+            # =============================================
+
+            # Raspagem de unidades em que o proc esta aberto
+            mudar_iframe('visualizacao')
+            time.sleep(1.5)
+
+            try:
+                unidade_andamento = driver.find_element("xpath", '//*[@id="capaProcessoPro"]/div[1]/div[2]').text
+            except:
+                time.sleep(1.5)
+                try:
+                    unidade_andamento = driver.find_element("xpath", '//*[@id="capaProcessoPro"]/div[1]/div[2]').text
+                except Exception as e:
+                    print(f"Erro persistente: {e}")
+                    unidade_andamento = "Erro ao buscar unidade_andamento"
+
+            
+            # Link permanente do processo
+            id_procedimento = driver.find_element("xpath", '//*[@id="capaProcessoPro"]/div[3]/div[2]/a[2]').get_attribute('data-id_procedimento')
+            link_proc = (env['LINK_PROC_MODELO'] if is_local() else st.secrets['LINK_PROC_MODELO']) + id_procedimento
+
+            # Raspagem de documendo de encerramento de processo
+            mudar_iframe('visualizacao')
+            driver.find_element("xpath", '//img[@alt="Gerar Arquivo PDF do Processo"]').click()
+            tabela_documentos = driver.find_element(By.ID, "tblDocumentos")
+
+            # Localizar as células na tabela de documentos que contêm o texto "Termo de Encerramento do Processo"
+            termos = len(tabela_documentos.find_elements(By.XPATH, ".//td[contains(text(), 'Termo de Encerramento do Processo')]"))
+            
+            if (termos > 0 and unidade_andamento == 'Processo não possui andamentos abertos.'):
+
+                conclusao = 'Concluído'
+
+            elif  (termos > 0 and unidade_andamento != 'Processo não possui andamentos abertos.'):
+
+                conclusao = 'Possui termo de encerramento mas está aberto em unidades'
+
+            elif (termos == 0 and unidade_andamento == 'Processo não possui andamentos abertos.'):
+
+                conclusao = 'Não possui andamentos abertos mas não possui termo de encerramento'
+
+            elif (termos == 0 and unidade_andamento != 'Processo não possui andamentos abertos.'):
+
+                conclusao = 'Em andamento'
+
+            else: 
+
+                conclusao = 'Não foi possível concluir se o processo está encerrado'
+
+            
+            mudar_iframe('arvore')
+            driver.find_element("xpath", '//*[@id="divConsultarAndamento"]/a/span').click()
+            mudar_iframe('visualizacao')
+            
+            # DADOS DA TABELA DE ANDAMENTO:
             processo_sei_format = num_processo_sei(driver.find_element(By.XPATH, '//*[@id="divInfraBarraLocalizacao"]').text) # coluna de Processo
             data_hora = driver.find_element(By.XPATH, '//*[@id="tblHistorico"]/tbody/tr[2]/td[1]').text  # Coluna de Data/Horário
             qnt_dias = str(cont_dias(data_hora, datetime.now()))
@@ -140,7 +213,7 @@ def buscar_dados_andamento(unidade, processos):
 
             # Atualiza os dados no DataFrame
             processos.loc[processos['Processos'] == processo, colunas] = [
-                processo_sei_format, data_hora, qnt_dias, unidade_abreviação, unidade_nome, usuario_cpf, usuario, descricao
+                processo_sei_format, data_hora, qnt_dias, conclusao, unidade_abreviação, unidade_nome, usuario_cpf, usuario, descricao, unidade_andamento, link_proc
             ]
 
             # Voltar ao contexto principal
@@ -154,6 +227,10 @@ def buscar_dados_andamento(unidade, processos):
     # ordenando as colunas
     processos = processos[colunas]
 
+    # organizando para o display
+    processos_display = processos.copy()
+    processos_display['Acessar Processo'] = processos_display['Link do Processo']
+  
     # Exportando em excel
     df_processos_xlsx = converter_para_excel(processos)
 
@@ -165,6 +242,19 @@ def buscar_dados_andamento(unidade, processos):
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
 
-    dataframe_final = st.dataframe(processos, hide_index=True)
 
+    dataframe_final = st.dataframe(processos_display,
+                                   column_config={
+                                       'Acessar Processo': st.column_config.LinkColumn(
+                                           'Ir ao Processo',
+                                            display_text="Acessar"
+                                       )
+                                   }, 
+                                   column_order=('Processos', 'Acessar Processo', 'Data Horário', 'Qnt. Dias', 'Concluído', 'Unidade Atual',
+                                                 'Nome Unidade Atual', 'Usuário CPF', 'Usuário', 'Descrição',
+                                                  'Processo aberto nas unidades', 'Link do Processo'
+                                         ), 
+                                     hide_index=True
+                                   )
+    
     return dataframe_final
--- utils/chrome.py
+++ utils/chrome.py
@@ -6,11 +6,11 @@ from utils.funcoes_auxiliares import *
 def chrome():
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
+    #chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_experimental_option('detach', True)
     chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-    chrome_options.add_extension('chromedriver/SEI Pro - Chrome Web Store 1.5.5.54.crx')
+    chrome_options.add_extension(r'chromedriver\SEI Pro - Chrome Web Store 1.5.5.54.crx')
 
     if is_local():
         service = Service('chromedriver.exe')
