fcc0bb7631113bb66acebb68c195606321b5d8a2
Vinicius Ventura <viniciuscventura@gmail.com>
2025-01-09 10:50:53 -0300

    teste chromedriver

--- utils/chrome.py
+++ utils/chrome.py
@@ -10,7 +10,7 @@ def chrome():
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_experimental_option('detach', True)
     chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-    chrome_options.add_argument('--load-extension=SEI Pro - Chrome Web Store 1.5.5.54.crx')
+    chrome_options.add_extension(r'SEI Pro - Chrome Web Store 1.5.5.54.crx')
 
     if is_local():
         service = Service('chromedriver.exe')
