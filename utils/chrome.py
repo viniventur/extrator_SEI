90d8a1dfb2e8f4aa3c0187cef2097949e293a2f9
Vinicius Ventura <viniciuscventura@gmail.com>
2025-01-30 14:28:33 -0300

    teste chrome config

--- utils/chrome.py
+++ utils/chrome.py
@@ -45,7 +45,7 @@ def chrome():
 
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
-    #chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_experimental_option('detach', True)
     chrome_options.add_argument('--disable-blink-features=AutomationControlled')
